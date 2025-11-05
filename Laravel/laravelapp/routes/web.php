9a493d5a52e2dcda18187b7a6a17573e6c831bab
usudon01 <sslive88@outlook.jp>
2025-11-05 22:52:34 +0900

    勉強進めた

--- Laravel/laravelapp/routes/web.php
+++ Laravel/laravelapp/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\HelloController;
+
+Route::get('hello', [HelloController::class, 'Index']);
+Route::get('hello/other', [HelloController::class, 'Other']);
